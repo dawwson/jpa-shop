f03909a4566c985b526eaa19b9a3f266c5a6f6c4
dawwson <shines1427@gmail.com>
2023-09-03 15:36:27 +0900

    Feat: 회원 서비스 비즈니스 로직 완성

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;  // 스프링이 제공하는 트랜잭션 사용 권장
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)  // 읽기전용 메서드에 사용
+@RequiredArgsConstructor  // final 붙은 의존 관계를 생성자 주입해줌
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional  // 쓰기를 하는 메서드에는 readOnly = false로 적용
+    public Long join(Member member) {
+        // 중복 회원 검증
+        validateMemeber(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 회원 검증
+    private void validateMemeber(Member member) {
+        List<Member> members = memberRepository.findByName(member.getName());
+
+        if (!members.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     * @return 모든 회원 리스트
+     */
+    public List<Member> findAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOneMember(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
