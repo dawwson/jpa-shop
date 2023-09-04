f163c07de9ecab46d5cd40b73c4e81e6d077efd5
dawwson <shines1427@gmail.com>
2023-09-04 13:45:55 +0900

    Test: findOne -> findById로 수정

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -34,7 +34,7 @@ class MemberServiceTest {
         Long savedId = memberService.join(member);
 
         // then
-        assertEquals(member, memberRepository.findOne(savedId));
+        assertEquals(member, memberRepository.findById(savedId));
     }
 
     @Test
